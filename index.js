acac2f8e18b7695e0244b55f01e5a8e948b67840
Wizkid <aaronjporter@gmail>
2024-03-17 10:27:08 -0700

    Fixed 404 page

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.get('/', (req, res) => {
     res.render('home'); 
 });
 
+app.get('*', (req, res) => {
+    res.render('error404');
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
