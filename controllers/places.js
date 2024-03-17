276ec636faf9830abc76f0caae70c032c94fdbc3
Wizkid <aaronjporter@gmail>
2024-03-16 19:24:27 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -2,35 +2,33 @@
 
 const express = require('express');
 const router = express.Router();
+const places = require('../models/places.js')
 
-// POST route for form submission
 router.post('/', (req, res) => {
-    console.log(req.body); // Here you would add your logic to save the data
-    // For now, let's just redirect to the GET '/places' route
-    res.redirect('/places');
-});
+    console.log(req.body)
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'images/sushi-cat.jpg'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
+
 
 // GET route to display the form for a new place
 router.get('/new', (req, res) => {
-    res.render('places/new'); // Make sure you have a 'new.jsx' view in your 'views/places' directory
+    res.render('places/new'); 
 });
 
 // GET route for the main list of places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/Wakuda.jpg'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/Gordon-Ramsay.jpg'
-    }];
-    res.render('places/index', { places }); // Make sure you have an 'index.jsx' view in your 'views/places' directory
+    res.render('places/index', { places });
 });
 
 module.exports = router;
