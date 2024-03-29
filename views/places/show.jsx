da070591345580ad0aeeb1f9e6ffbb7ef960448c
Wizkid <aaronjporter@gmail>
2024-03-28 18:39:47 -0700

    Added Star Rating Calculation

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,6 +9,28 @@ function show(data) {
             No comments yet!
         </h3>
     );
+
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
+    if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
+    }
+
     if (data.place.comments.length) {
         comments = data.place.comments.map((c, index) => (
             <div key={index} className="border">
@@ -21,7 +43,6 @@ function show(data) {
             </div>
         ));
     }
-
     return (
         <Def>
             <main>
@@ -39,7 +60,7 @@ function show(data) {
                 <h1>{data.place.name}</h1>
                 <div>
                     <h2>Rating</h2>
-                    Currently unrated
+                    {rating}
                 </div>
                 <div>
                     <h2>Comments</h2>
