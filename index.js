8f677e020b0ee10f3366e499b3ebc235297078bb
Wizkid <aaronjporter@gmail>
2024-04-04 19:23:29 -0700

    Updated port info

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ app.get('*', (req, res) => {
     res.render('error404');
 });
 
-// Start the server
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const port = process.env.PORT; 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 module.exports = app;
